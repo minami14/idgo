e5d31020c44ef6e97bcbe81e787f5335a46dd4c6
minami14 <minami-git@minami-vr.sakura.ne.jp>
2019-07-14 06:57:46 +0900

    add server test

--- idgo/generator_test.go
+++ idgo/generator_test.go
@@ -1,20 +1,52 @@
 package idgo
 
 import (
+	"log"
 	"math"
+	"net"
+	"os"
 	"sync"
 	"testing"
+	"time"
 )
 
-func TestGenerateID(t *testing.T) {
-	gen, err := NewIDGenerator(math.MaxInt16)
+const address = ":4000"
+
+func RunServer(t *testing.T) {
+	tcpAddr, err := net.ResolveTCPAddr("tcp", address)
 	if err != nil {
-		t.Error(err)
+		log.Fatal(err)
 	}
+	logger := log.New(os.Stdout, "", log.LstdFlags)
+	s, err := NewServer(math.MaxInt16, tcpAddr, logger)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		if err := s.Run(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	time.Sleep(1 * time.Second)
+}
+
+func TestGenerateIDByServer(t *testing.T) {
+	RunServer(t)
+
+	tcpAddr, err := net.ResolveTCPAddr("tcp", address)
+	if err != nil {
+		log.Fatal(err)
+	}
+	client := NewClient()
+	if err := client.Connect(tcpAddr); err != nil {
+		log.Fatal(err)
+	}
+
 	used := make([]bool, math.MaxInt16)
 	m := &sync.Mutex{}
 	for i := 0; i < math.MaxInt16; i++ {
-		id, err := gen.Generate()
+		id, err := client.Generate()
 		if err != nil {
 			t.Error(err)
 		}
@@ -22,12 +54,15 @@ func TestGenerateID(t *testing.T) {
 			t.Errorf("invalid id %b", id)
 			return
 		}
-		gen.Free(id)
+		if err := client.Free(id); err != nil {
+			t.Error(err)
+		}
 	}
+
 	for i := 0; i < 100; i++ {
 		go func() {
 			for j := 0; j < 100; j++ {
-				id, err := gen.Generate()
+				id, err := client.Generate()
 				if err != nil {
 					t.Error(err)
 				}
