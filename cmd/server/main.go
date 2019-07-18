3bdb118696177b95c16640e56004d7b9201653a8
minami14 <minami-git@minami-vr.sakura.ne.jp>
2019-07-18 19:12:49 +0900

    add command line options

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,27 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"math"
 	"net"
 	"os"
 
+	"github.com/jessevdk/go-flags"
 	"github.com/minami14/idgo/idgo"
 )
 
+type Options struct {
+	MaxSize int    `short:"m" long:"max" description:"Maximum value of ID to be generated" default:"2147483647"`
+	Port    uint16 `short:"p" long:"port" description:"Port number" default:"49152"`
+}
+
 func main() {
-	tcpAddr, err := net.ResolveTCPAddr("tcp", ":4000")
+	var options Options
+	if _, err := flags.Parse(&options); err != nil {
+		os.Exit(1)
+	}
+
+	addr := fmt.Sprintf(":%d", options.Port)
+	tcpAddr, err := net.ResolveTCPAddr("tcp", addr)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	s, err := idgo.NewServer(math.MaxInt16, tcpAddr)
+	s, err := idgo.NewServer(options.MaxSize, tcpAddr)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	s.SetLogger(log.New(os.Stdout, "", log.LstdFlags))
+	s.SetLogger(log.New(os.Stdout, "idgo ", log.LstdFlags))
 
+	fmt.Println("idgo server started")
 	if err := s.Run(); err != nil {
 		log.Fatal(err)
 	}
